c456117be60411aa2df6ac014ccbbd61d3815931
rerearitet <37345277+rerearitet@users.noreply.github.com>
2020-10-31 14:45:42 +0200

    Update estate.sql

--- src/main/java/sql/estate.sql
+++ src/main/java/sql/estate.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS estate (
-                                                        id int not null,
-                                                        name varchar(100) not null
-                  );
+  id int not null,
+  name varchar(100) not null
+);
 
 INSERT INTO estate (id, name) VALUES
 (1, 'Estate1'),
